de37f3a8bd9fa19dbde98ba7c42e0a9d8dd5d9c0
Sergio <sergiomsrs@gmail.com>
2024-09-18 07:55:48 +0200

    Actualizar redirección de rutas en MainPage.jsx y 404.html

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './styles.css'
-import {  BrowserRouter, HashRouter} from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import { MainPage } from './pages/MainPage'
 import { AppProvider } from './context/AppProvider'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppProvider>
         <MainPage />
       </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
