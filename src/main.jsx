322f2b26bfdb62d59377b71b9b7181a0e5ef4093
Sergio <sergiomsrs@gmail.com>
2024-09-18 06:51:08 +0200

    cambiado a broserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './styles.css'
-import {  HashRouter } from 'react-router-dom'
+import {  BrowserRouter} from 'react-router-dom'
 import { MainPage } from './pages/MainPage'
 import { AppProvider } from './context/AppProvider'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <MainPage />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
