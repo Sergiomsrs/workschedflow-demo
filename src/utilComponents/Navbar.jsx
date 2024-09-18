f6872e02e0f4278557f6ac85759bf8faa2096ed3
Sergio <sergiomsrs@gmail.com>
2024-09-18 07:33:37 +0200

    Actualizar rutas de imagen en Navbar.jsx

--- src/utilComponents/Navbar.jsx
+++ src/utilComponents/Navbar.jsx
@@ -6,13 +6,13 @@ export const Navbar = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between h-16">
         <div className="flex items-center">
-          <Link to="/landing" className="text-gray-300 text-lg hover:text-white px-3 py-2 font-medium">
+          <Link to="/workschedflow-demo/landing" className="text-gray-300 text-lg hover:text-white px-3 py-2 font-medium">
             Información
           </Link>
-          <Link to="/" className="text-gray-300 hover:text-white px-3 py-2 text-lg font-medium">
+          <Link to="/workschedflow-demo/" className="text-gray-300 hover:text-white px-3 py-2 text-lg font-medium">
             Horarios
           </Link>
-          <Link to="/employeeweek" className="text-gray-300 hover:text-white px-3 py-2 text-lg font-medium">
+          <Link to="/workschedflow-demo/employeeweek" className="text-gray-300 hover:text-white px-3 py-2 text-lg font-medium">
             Resumen Semanal
           </Link>
           {/*<Link to="/adduser" className="text-gray-300 hover:text-white px-3 py-2 text-lg font-medium">
