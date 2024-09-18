2b8b8e287943d29d22f7217707ea3336bbcfbdea
Sergio <sergiomsrs@gmail.com>
2024-09-18 08:09:43 +0200

    Actualizar redirección de rutas en Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -76,7 +76,7 @@ export const Landing = () => {
         <section className="mt-10">
           <h3 className="text-2xl font-semibold mb-4">Conoce la App: Versión Demo</h3>
         <section className="flex justify-start gap-4 mb-4">
-          <LinkButton to={"/"}>Ir a la Demo</LinkButton>
+          <LinkButton to={"/#/workschedflow-demo/"}>Ir a la Demo</LinkButton>
           <LinkButton to={"/workschedflow-demo/techinfo"}>Detrás del Desarrollo</LinkButton>
         </section>
           <Projects
