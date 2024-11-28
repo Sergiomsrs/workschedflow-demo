e3876b54be476e70dd0f072eae84a1c13f396e28
Sergio <sergiomsrs@gmail.com>
2024-11-28 09:30:43 +0100

    Bug clickar y arrastrar corregido

--- src/gridComponents/HorizontalBar .jsx
+++ src/gridComponents/HorizontalBar .jsx
@@ -2,11 +2,7 @@ import React, { useState } from "react";
 import { entrada } from "../utils/data";
 import { formatTime, getHighestNonZeroIndex } from "../utils/function";
 
-export const HorizontalBar = ({ username, hours, onHourChange, teamWork, shiftDurationes, phours }) => {
-
-  const [startSelection, setStartSelection] = useState(null);
-  const [isSelecting, setIsSelecting] = useState(false);
-
+export const HorizontalBar = ({ username, hours, onHourChange, teamWork, shiftDurationes, phours, isSelecting, setIsSelecting, startSelection, setStartSelection, handleMouseUp, id }) => {
 
   const handleMouseDown = (index) => {
     setStartSelection(index);
@@ -17,18 +13,18 @@ export const HorizontalBar = ({ username, hours, onHourChange, teamWork, shiftDu
     if (isSelecting && startSelection !== null) {
       const selectionStart = Math.min(startSelection, index);
       const selectionEnd = Math.max(startSelection, index);
-      const isStartSelected = hours[startSelection] !== "Null";
+      const isStartSelected = hours[startSelection] !== "Null" || hours[startSelection] === "PTO";
 
       for (let i = selectionStart; i <= selectionEnd; i++) {
+        if (isInputDisabled(i)) {
+          continue;
+        }
         onHourChange(i, isStartSelected ? entrada[i] : "Null");
       }
     }
   };
 
-  const handleMouseUp = () => {
-    setIsSelecting(false);
-    setStartSelection(null);
-  };
+
 
   const handleClick = (index, isChecked) => {
     onHourChange(index, isChecked ? entrada[index] : "Null");
@@ -43,23 +39,26 @@ export const HorizontalBar = ({ username, hours, onHourChange, teamWork, shiftDu
   return (
     <>
       <td className="text-base font-semibold text-gray-800">{teamWork}</td>
-      <td className="text-base font-semibold text-gray-800">{username}</td>
+      <td 
+      className="text-base font-semibold text-gray-800">
+        <button onClick={()=>console.log(username, teamWork, id)} >{username}</button>
+        </td>
       {hours && hours.map((value, index) => (
         <td
           className={`relative md:w-24 w-12 text-center align-bottom ${index % 4 === 3 ? 'border-45-height' : ''}`}
           key={index}
-          onMouseDown={() => handleMouseDown(index)}
-          onMouseEnter={() => handleMouseEnter(index)}
-          onMouseUp={handleMouseUp}
         >
           <input
             type="checkbox"
-            className={`w-4 h-4 p-0 m-0 appearance-none border border-gray-400 rounded-3xl ${isInputDisabled(index) || value === "NP" ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'
-              } ${value !== "Null" && value !== "NP" ? 'bg-indigo-500' : 'bg-neutral-200'
+            className={`w-4 h-4 p-0 m-0 appearance-none border border-gray-400 rounded-3xl ${isInputDisabled(index) || value === "PTO" ? 'cursor-not-allowed opacity-30' : 'cursor-pointer'
+              } ${value !== "Null" && value !== "PTO" ? 'bg-indigo-500' : 'bg-neutral-200'
               } relative`}
-            checked={value !== "Null" && value !== "NP"}
+            checked={value !== "Null" && value !== "PTO"}
             onChange={(event) => handleClick(index, event.target.checked)}
-            disabled={isInputDisabled(index) || value === "NP"}
+            disabled={isInputDisabled(index) || value === "PTO"}
+            onMouseDown={() => handleMouseDown(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseUp={handleMouseUp}
           />
           {index % 4 === 3 && (
             <div className="absolute right-0 top-1/2 h-1/2 bg-slate-400" style={{ zIndex: -1, width: 1 }}></div>
--- src/gridComponents/JobHourApp.jsx
+++ src/gridComponents/JobHourApp.jsx
@@ -1,33 +1,51 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { HorizontalBar } from "./HorizontalBar ";
 import { AppContext } from "../context/AppContext";
 import { DistributionBar } from "./DistributionBar";
 
-export const JobHourApp = ({ employees, onHourChange, day, dayIndex, eh }) => {
+export const JobHourApp = ({ employees, onHourChange, day, eh }) => {
+
 
   const { selectedOption } = useContext(AppContext);
+  const [startSelection, setStartSelection] = useState(null);
+  const [isSelecting, setIsSelecting] = useState(false);
+
+  const handleMouseUp = () => {
+    setIsSelecting(false);
+    setStartSelection(null);
+  };
 
   return (
     <>
-      <tbody>
-        {employees.map((employee, employeeIndex) => (
-          <tr key={employee.name}>
-            {(selectedOption === "todos" || selectedOption === employee.teamWork) && (
-              <HorizontalBar
-                teamWork={employee.teamWork}
-                day={day}
-                dayIndex={dayIndex}
-                username={employee.name}
-                shiftDurationes={employee.shiftDuration}
-                phours={eh[employeeIndex].workShift}
-                hours={employee.workShift}
-                onHourChange={(hourIndex, value) =>
-                  onHourChange(employeeIndex, hourIndex, value)
-                }
-              />
-            )}
-          </tr>
-        ))}
+     <tbody>
+        {employees.map((employee, employeeIndex) => {
+          // Buscar el turno del día anterior usando el ID del empleado
+          const previousShift = eh.find(prevEmployee => prevEmployee.id === employee.id);
+
+          return (
+            <tr key={employee.id} onMouseLeave={handleMouseUp}>
+              {(selectedOption === "todos" || selectedOption === employee.teamWork) && (
+                <HorizontalBar
+                id={employee.id}
+                  teamWork={employee.teamWork}
+                  username={employee.name}
+                  shiftDurationes={employee.shiftDuration}
+                  // Si no se encuentra, usar un array de 62 elementos "Null"
+                  phours={previousShift ? previousShift.workShift : Array(62).fill("Null")}
+                  hours={employee.workShift}
+                  startSelection={startSelection}
+                  isSelecting={isSelecting}
+                  setIsSelecting={setIsSelecting}
+                  setStartSelection={setStartSelection}
+                  handleMouseUp={handleMouseUp}
+                  onHourChange={(hourIndex, value) =>
+                    onHourChange(employeeIndex, hourIndex, value)
+                  }
+                />
+              )}
+            </tr>
+          );
+        })}
         <DistributionBar day={day} />
       </tbody>
     </>
